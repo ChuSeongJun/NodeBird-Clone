91b28bf04ad19540104a21682e374d072883c31a
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-02-03 15:35:57 +0900

    Feat: Head 추가

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppLayout from "../components/AppLayout";
+import Head from "next/head";
 
 const SignUp = () =>{
     return(
+        <>
+            <Head>
+                <title>회원가입 | NodeBird</title>
+            </Head>
         <AppLayout>
             <div>회원가입페이지</div>
         </AppLayout>
+            </>
     )
 }
 
