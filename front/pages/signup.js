852e50b39264fadd5eab20ec29f1e2d2da523463
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-02-27 23:44:06 +0900

    ?

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState } from "react";
+import Head from "next/head";
+import { Button, Checkbox, Form, Input } from "antd";
 
-import AppLayout from '../components/AppLayout';
-import useInput from '../hooks/useInput';
+import AppLayout from "../components/AppLayout";
+import useInput from "../hooks/useInput";
 
 const Signup = () => {
-  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState("");
   const [term, setTerm] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const [termError, setTermError] = useState(false);
 
-  const [id, onChangeId] = useInput('');
-  const [nick, onChangeNick] = useInput('');
-  const [password, onChangePassword] = useInput('');
+  const [id, onChangeId] = useInput("");
+  const [nick, onChangeNick] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
   const onSubmit = () => {
     if (password !== passwordCheck) {
@@ -32,7 +32,7 @@ const Signup = () => {
       term,
     });
   };
-
+//
   const onChangePasswordCheck = (e) => {
     setPasswordError(e.target.value !== password);
     setPasswordCheck(e.target.value);
@@ -44,49 +44,68 @@ const Signup = () => {
   };
 
   return (
-      <>
-        <Head>
-          <title>NodeBird</title>
-        </Head>
-        <AppLayout>
-          <Form onFinish={onSubmit} style={{ padding: 10 }}>
-            <div>
-              <label htmlFor="user-id">아이디</label>
-              <br />
-              <Input name="user-id" value={id} required onChange={onChangeId} />
-            </div>
-            <div>
-              <label htmlFor="user-nick">닉네임</label>
-              <br />
-              <Input name="user-nick" value={nick} required onChange={onChangeNick} />
-            </div>
-            <div>
-              <label htmlFor="user-password">비밀번호</label>
-              <br />
-              <Input name="user-password" type="password" value={password} required onChange={onChangePassword} />
-            </div>
-            <div>
-              <label htmlFor="user-password-check">비밀번호체크</label>
-              <br />
-              <Input
-                  name="user-password-check"
-                  type="password"
-                  value={passwordCheck}
-                  required
-                  onChange={onChangePasswordCheck}
-              />
-              {passwordError && <div style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</div>}
-            </div>
-            <div>
-              <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>성준이 말을 잘 들을 것을 동의합니다.</Checkbox>
-              {termError && <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>}
-            </div>
-            <div style={{ marginTop: 10 }}>
-              <Button type="primary" htmlType="submit">가입하기</Button>
-            </div>
-          </Form>
-        </AppLayout>
-      </>
+    <>
+      <Head>
+        <title>NodeBird</title>
+      </Head>
+      <AppLayout>
+        <Form onFinish={onSubmit} style={{ padding: 10 }}>
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <Input name="user-id" value={id} required onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-nick">닉네임</label>
+            <br />
+            <Input
+              name="user-nick"
+              value={nick}
+              required
+              onChange={onChangeNick}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <Input
+              name="user-password"
+              type="password"
+              value={password}
+              required
+              onChange={onChangePassword}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호체크</label>
+            <br />
+            <Input
+              name="user-password-check"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={onChangePasswordCheck}
+            />
+            {passwordError && (
+              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              성준이 말을 잘 들을 것을 동의합니다.
+            </Checkbox>
+            {termError && (
+              <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
+            )}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
+          </div>
+        </Form>
+      </AppLayout>
+    </>
   );
 };
 
